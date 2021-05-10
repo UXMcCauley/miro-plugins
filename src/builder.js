5e34f7330fa78e3623eff903d239eda4bb166270
Andrew McCauley <uxmccauley@gmail.com>
2021-05-10 12:19:24 -0500

    Removed useless clear input file function.

--- src/builder.js
+++ src/builder.js
@@ -43,21 +43,6 @@ function makeDataArray (data) {
   return allData;
 }
 
-function clearInputFile(f){
-    if(f.value){
-        try{
-            f.value = ''; //for IE11, latest Chrome/Firefox/Opera...
-        }catch(err){ }
-        if(f.value){ //for IE5 ~ IE10
-            var form = document.createElement('form'),
-                parentNode = f.parentNode, ref = f.nextSibling;
-            form.appendChild(f);
-            form.reset();
-            parentNode.insertBefore(f,ref);
-        }
-    }
-}
-
 function mapColor (color) {
   let stickyColor;
   switch (color) {
