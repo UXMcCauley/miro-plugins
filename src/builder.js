44e648d32cd7b2362bfc8566aebf6e1f9ec4e94a
Andrew McCauley <uxmccauley@gmail.com>
2021-05-10 09:27:44 -0500

    New SVGs and error messaging

--- src/builder.js
+++ src/builder.js
@@ -1,13 +1,21 @@
 $('#form').on('submit', function (e) {
-  console.log('submit button clicked')
   e.preventDefault();
 
   var file = document.getElementById('fileItem').files[0];
+  if(file){
+    Papa.parse(file, {
+      complete: function (results) {
+        if(results) {
+          console.log(results)
+          console.log("has results")
+        } else {
+          console.log("something went wrong")
+        }
+      }
+    });
+  } else {
+    $('span').addClass('errorText');
+  }
 
-  Papa.parse(file, {
-    complete: function (results) {
-      console.log(results)
-    }
-  });
 
 })
