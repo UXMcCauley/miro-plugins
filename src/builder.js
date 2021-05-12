0570ae608b692221b2aa3c36876f0944731b2abd
Andrew McCauley <uxmccauley@gmail.com>
2021-05-11 19:27:33 -0500

    Added test button and ehandler

--- src/builder.js
+++ src/builder.js
@@ -1,12 +1,13 @@
-
-    miro.board.widgets.create({
-      "type": "sticker",
-      "style": {
-        "backgroundColor": "#ea94bb",
-        "fontFamily": "Bangers",
-        "textAlign": "right"
-      },
-      "text": "Test Sticker Here",
-      "x": 0,
-      "y": 0
-    })
+$('.testClick').on("click", function () {
+  miro.board.widgets.create({
+    "type": "sticker",
+    "style": {
+      "backgroundColor": "#ea94bb",
+      "fontFamily": "Bangers",
+      "textAlign": "right"
+    },
+    "text": "Test Sticker Here",
+    "x": 0,
+    "y": 0
+  })
+})
