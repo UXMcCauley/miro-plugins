b04c10bd56160eea1e7cf29afc6cd0c8bbf69b60
Andrew McCauley <uxmccauley@gmail.com>
2021-05-12 13:14:34 -0500

    removed style obj from creat widget

--- src/builder.js
+++ src/builder.js
@@ -2,11 +2,6 @@ $('#testClick').on("click", function () {
   console.log(this);
   miro.board.widgets.create({
     "type": "sticker",
-    "style": {
-      "backgroundColor": "#ea94bb",
-      "fontFamily": "Bangers",
-      "textAlign": "right"
-    },
     "text": "Test Sticker Here",
     "x": 0,
     "y": 0
