7dace50bbe9abe88e8f4ea3b46afddbe84eca773
Andrew McCauley <uxmccauley@gmail.com>
2021-05-12 13:29:16 -0500

    v1 string

--- src/builder.js
+++ src/builder.js
@@ -1,12 +1,12 @@
 $('#testClick').on("click", function () {
-  console.log(v1);
+  console.log("v1");
   miro.board.widgets.create({
     "type": "sticker",
     "text": "Test Sticker Here",
     "x": 0,
     "y": 0,
     "style": {
-      "background-color": "#ea94bb"
+      "backgroundColor": "#ea94bb"
     }
   })
 })
